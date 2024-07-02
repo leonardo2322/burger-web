c85131a8c8c185428e69bf14a056db402eeb9e8e
unknown <joys23services@gmail.com>
2024-07-01 22:19:01 -0400

    erro de localstorage

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,8 +6,25 @@ import { navBarMenu, scrollToTop } from "./components/navBar.js"
 import { renderProducts } from "./components/products.js"
 import { sendMessage } from "./components/sendMessageInfo.js"
 import { swiper } from "./helpers/swipperJS.js"
-
+import { db } from "./components/products.js"
 document.addEventListener('DOMContentLoaded', () => {
+    var itemsJSON = JSON.stringify(db.items);
+
+    // Obtener el contenido de localStorage si existe
+    var productsJSON = localStorage.getItem('products');
+  
+    // Comparar el contenido de items con el almacenado en localStorage
+    if (productsJSON === null || productsJSON !== itemsJSON) {
+      // Si no existe en localStorage o es diferente, actualizamos localStorage
+      localStorage.setItem('products', itemsJSON);
+      console.log('Se ha actualizado el contenido de items en localStorage.');
+    } else {
+      // Si son iguales, no hacemos nada
+      console.log('El contenido de items en localStorage es igual al del archivo JS.');
+    }
+  
+
+
     swiper
     navBarMenu()
     scrollToTop()
